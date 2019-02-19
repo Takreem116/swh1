abd7691a3a370446f8af5a5497e09f0cb59de100
تكريم <@192.168.1.94>
2019-02-19 23:25:19 +0200

    hi

---
+++ src/SWHW/book.java
@@ -0,0 +1,24 @@
+package SWHW;
+
+public class book {
+int p;
+String nameOfBook;
+	public book(String name, int price) {
+	nameOfBook=name;
+	p=price;
+	// TODO Auto-generated constructor stub
+	
+	
+	
+	}
+
+	public int getp() {
+		// TODO Auto-generated method stub
+		return p;
+	}
+
+	
+
+	
+
+}
